94ff7e6c8a60dfcb56a9dcad463be5db3796fa59
Miles Ornish <36129525+milesan@users.noreply.github.com>
2024-11-23 04:45:21 +0800

    Update synesthesia/newcalx/tailwind.config.js [BoltSync]

--- synesthesia/newcalx/tailwind.config.js
+++ synesthesia/newcalx/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Space Mono', 'monospace'],
+        display: ['VT323', 'monospace'],
+        serif: ['Playfair Display', 'serif'],
+        body: ['Lora', 'serif']
+      }
+    },
   },
   plugins: [],
 };
